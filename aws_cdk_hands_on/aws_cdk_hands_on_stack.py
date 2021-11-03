9d13ae2efed17dfd9e47c4069dbbb93584710fb1
gyvm <jolcde75+github@gmail.com>
2021-11-04 00:44:45 +0900

    Lambdaを追加

--- aws_cdk_hands_on/aws_cdk_hands_on_stack.py
+++ aws_cdk_hands_on/aws_cdk_hands_on_stack.py
@@ -1,9 +1,8 @@
-from aws_cdk import core as cdk
+from aws_cdk import (
+    core as cdk, 
+    aws_lambda as _lambda,
+    )
 
-# For consistency with other languages, `cdk` is the preferred import name for
-# the CDK's core module.  The following line also imports it as `core` for use
-# with examples from the CDK Developer's Guide, which are in the process of
-# being updated to use `cdk`.  You may delete this import if you don't need it.
 from aws_cdk import core
 
 
@@ -12,4 +11,9 @@ class AwsCdkHandsOnStack(cdk.Stack):
     def __init__(self, scope: cdk.Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        hello = _lambda.Function(
+            self, 'HelloHandler',
+            runtime=_lambda.Runtime.PYTHON_3_7,
+            code=_lambda.Code.asset('lambda'),
+            handler='hello.handler',
+        )
